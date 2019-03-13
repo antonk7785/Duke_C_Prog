21c9168552f0b4d48ae530e8d43c9c54fb6f8410
Coursera Learner <nobody@nowhere.nul>
2019-03-13 04:52:22 +0000

    power done

---
+++ 23_power_rec/test-power.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power (unsigned x, unsigned y);
+
+int main(void)
+{
+  if (power(0, 0) != 1)
+    return EXIT_FAILURE;
+  if (power(1, 1) != 1)
+    return EXIT_FAILURE;
+  if (power(2, 2) != 4)
+    return EXIT_FAILURE;
+  if (power(4, 10) != 1048576)
+    return EXIT_FAILURE;
+  if (power(-1, 10) != 1)
+    return EXIT_FAILURE;
+
+  
+  return EXIT_SUCCESS;
+}
