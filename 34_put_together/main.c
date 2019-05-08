f30b4a302594271cd001c76b827778ccf0ef3d6b
Coursera Learner <nobody@nowhere.nul>
2019-05-08 01:05:57 +0000

    count values is done

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -5,34 +5,68 @@
 #include "counts.h"
 #include "outname.h"
 
-counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
-  //WRITE ME
-  return NULL;
+counts_t * countFile(const char * filename, kvarray_t * kvPairs) 
+{
+  counts_t * answer = createCounts();
+  FILE * f = fopen(filename, "r");
+  if (f == NULL)
+  {
+    return NULL;
+  }
+
+  char * line = NULL;
+  size_t sz = 0;
+  while (getline(&line, &sz, f) >= 0)
+  {
+    char * str = strchr(line, '\n');
+    if (str != NULL)
+    {
+      *str = '\0';
+    }
+
+    char * x = lookupValue(kvPairs, line);
+    addCount(answer,x);
+  }
+  free(line);
+  if (fclose(f) != 0)
+  {
+    return NULL;
+  }
+
+  return answer;
 }
 
-int main(int argc, char ** argv) {
-  //WRITE ME (plus add appropriate error checking!)
- //read the key/value pairs from the file named by argv[1] (call the result kv)
+int main(int argc, char ** argv) 
+{
+  if(argc < 2)
+  {
+    fprintf(stderr, "no enough arguments");
+    return EXIT_FAILURE;
+  }
 
- //count from 2 to argc (call the number you count i)
+  kvarray_t * kv = readKVs(argv[1]);
 
-    //count the values that appear in the file named by argv[i], using kv as the key/value pair
-    //   (call this result c)
+  for(int i = 2; i < argc; i++)
+  {
+    counts_t * c = countFile(argv[i], kv);
+    if(c == NULL) 
+    {
+      return EXIT_FAILURE;
+    }
+    char * outName = computeOutputFileName(argv[i]);
 
-    //compute the output file name from argv[i] (call this outName)
+    FILE * f = fopen(outName, "w");
 
-
-    //open the file named by outName (call that f)
-
-    //print the counts from c into the FILE f
-
-    //close f
-
-    //free the memory for outName and c
-
-
-
- //free the memory for kv
+    printCounts(c, f);
+    if (fclose(f) != 0 ) 
+    {  
+      return EXIT_FAILURE ;
+    }
+    free(outName);
+    freeCounts(c);
+  }
+  
+  freeKVs(kv);
 
   return EXIT_SUCCESS;
 }
